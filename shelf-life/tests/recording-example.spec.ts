ac05038cacf4df9e875bac2536c0175508fcbc1a
gabriel <gabrielcmoris@gmail.com>
2026-07-19 12:15:22 +0200

    Update HAR recording for OpenLibrary search and enhance test assertions
    - Modified the HAR recording for OpenLibrary to reflect updated request timings and headers.
    - Adjusted the test script to use the correct path for the HAR file.
    - Added an assertion to verify the visibility of the 'Brave New World' heading after performing a search.

--- shelf-life/tests/recording-example.spec.ts
+++ shelf-life/tests/recording-example.spec.ts
@@ -1,18 +1,14 @@
-import { test } from '@playwright/test';
-
-test.use({
-	serviceWorkers: 'block',
-	storageState: './playwright/.authentication/user.json'
-});
+import { test, expect } from '@playwright/test';
 
 test('Search', async ({ page }) => {
-	await page.routeFromHAR('playwright/recordings/openlibrary.har', {
+	await page.routeFromHAR('./playwright/recordings/openlibrary.har', {
 		url: '**/openlibrary.org/**'
 		// update: true // If I want to update the HAR recording of http network reqs
 	});
 	await page.goto('/search');
-	await page.getByRole('link', { name: 'Search' }).click();
 	await page.getByRole('textbox', { name: 'Search by title, author, or' }).click();
 	await page.getByRole('textbox', { name: 'Search by title, author, or' }).fill('Huxley');
 	await page.getByRole('button', { name: 'Search' }).click();
+
+	await expect(page.getByRole('heading', { name: 'Brave New World', exact: true })).toBeVisible();
 });
